63c8d8dc7341b5f45c59fed9bbc7e48fb8efc8e0
Jesus462 <jesusponce.462@gmail.com>
2020-08-12 19:03:17 -0400

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+//import react into the bundle
+import React from "react";
+import ReactDOM from "react-dom";
+
+//include bootstrap npm library into the bundle
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+//include your index.scss file into the bundle
+import "../styles/index.scss";
+
+//import your own components
+import Layout from "./Layout";
+
+//render your react application
+ReactDOM.render(<Layout />, document.querySelector("#app"));
---
+++ src/js/store/flux.js
@@ -0,0 +1,13 @@
+const getState = ({ getStore, getActions, setStore }) => {
+	return {
+		store: {},
+		actions: {
+			// Use getActions to call a function within a fuction
+			exampleFunction: () => {
+				getActions().changeColor(0, "green");
+			}
+		}
+	};
+};
+
+export default getState;
